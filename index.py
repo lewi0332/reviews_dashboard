32cd826fc3cf90409b2c496e5a1b4359f99302f4
Derrick Lewis <derrickjameslewis@gmail.com>
2023-11-29 19:15:51 -0600

    modal fix

--- index.py
+++ index.py
@@ -10,10 +10,11 @@ import os
 from dotenv import load_dotenv
 from dash import  dcc, html
 from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 import dash_auth
 import plotly.io as pio
 import dash_bootstrap_components as dbc
-# from google.cloud import secretmanager
+from google.cloud import secretmanager
 from plotly_theme_light import plotly_light
 from main import server, app
 from apps import home, page1, page2
@@ -26,22 +27,20 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # Access environment variables
 
-# PROJECT_ID = os.getenv('GCP_PROJECT')
-# print(PROJECT_ID)
+PROJECT_ID = os.getenv('GCP_PROJECT')
+print(PROJECT_ID)
 
 
-# def access_secret_version(secret_id, version_id="latest"):
-#     client = secretmanager.SecretManagerServiceClient()
-#     name = f"projects/{PROJECT_ID}/secrets/{secret_id}/versions/{version_id}"
-#     response = client.access_secret_version(name=name)
-#     return response.payload.data.decode('UTF-8')
+def access_secret_version(secret_id, version_id="latest"):
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{PROJECT_ID}/secrets/{secret_id}/versions/{version_id}"
+    response = client.access_secret_version(name=name)
+    return response.payload.data.decode('UTF-8')
 
 
-# UN = access_secret_version("UN")
-# PW = access_secret_version("PW")
+UN = access_secret_version("UN")
+PW = access_secret_version("PW")
 
-# UN = "admin"
-# PW = "admin"
 
 # VALID_USERNAME_PASSWORD_PAIRS = {
 #     UN: PW
@@ -66,7 +65,53 @@ dropdown_disc = dbc.DropdownMenu(
     in_navbar=True,
     label="Pages",
 )
+modal = html.Div(
+    [
+        dbc.Modal(
+            [
+                dbc.ModalHeader(
+                    dbc.ModalTitle("Login"),
+                    close_button=False
+                ),
+                dbc.ModalBody([
+                    html.Br(),
+                    html.Br(),
+                    html.Br(),
+                    html.Br(),
+                    html.Br(),
+                    html.Br(),
+                    dcc.Input(
+                        id="username",
+                        type="text",
+                        placeholder="username",
+                    ),
+                    html.Br(),
+                    html.Br(),
+                    dcc.Input(
+                        id="password",
+                        type="password",
+                        placeholder="password",
+                    ),
+                    html.Br(),
+                    html.Br(),
+                    html.Br(),
 
+                ]
+                ),
+                dbc.ModalFooter(
+                    dbc.Button(
+                        "Login", id="login_btn", className="ms-auto", n_clicks=0
+                    )
+                ),
+            ],
+            id="modal",
+            size="xl",
+            backdrop="static",
+            className="no-close",
+            is_open=True,
+        ),
+    ]
+)
 navbar = dbc.Navbar(
     dbc.Container([
         html.A(
@@ -161,12 +206,14 @@ for i in [2]:
 app.layout = html.Div([
     dcc.Location(id='url', refresh=True),
     navbar,
+    modal,
     html.Div(id='page-content', style={
         'margin-right': '70px',
         'margin-left': '50px'
     }),
     html.Div(
         [
+
             dcc.Markdown("""
                         Updated on November 2023
                         """,
@@ -203,6 +250,22 @@ def display_page(pathname):
         return page2.layout
     else:
         return '404'
+
+
+@app.callback(
+    Output('modal', 'is_open'),
+    [Input('login_btn', 'n_clicks')],
+    [State('username', 'value'),
+     State('password', 'value')])
+def usernamecall(clicks, username, password):
+    if clicks == 0 or clicks is None:
+        raise PreventUpdate
+
+    if username == UN and password == PW:
+        return False
+    else:
+        return True
+
 
 
 if __name__ == '__main__':
